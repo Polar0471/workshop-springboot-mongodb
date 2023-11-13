58575c57abdce6f0a4089747467ddb73f16e5fe9
Polar0471 <rodrigoarth512@gmail.com>
2023-11-13 20:20:54 -0300

    Implementando padrão DTO para retornar usuários

---
+++ src/main/java/com/polar/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package com.polar.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.polar.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable {
+    
+    private static final Long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
