0cd86e4cf37cf453829775fa2f4f77f39c942580
Polar0471 <rodrigoarth512@gmail.com>
2023-11-14 13:18:03 -0300

    Implementando deleção de usuário com DELETE

--- src/main/java/com/polar/workshopmongo/services/UserService.java
+++ src/main/java/com/polar/workshopmongo/services/UserService.java
@@ -31,6 +31,11 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
     // vai pegar um dto e instanciar um usuário
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
