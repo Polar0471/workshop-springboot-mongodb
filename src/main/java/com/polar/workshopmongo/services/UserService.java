00ea4034881509d85cc650f2e67e465522fae5ab
Polar0471 <rodrigoarth512@gmail.com>
2023-11-14 13:32:39 -0300

    Implementando atualização de usuário com PUT

--- src/main/java/com/polar/workshopmongo/services/UserService.java
+++ src/main/java/com/polar/workshopmongo/services/UserService.java
@@ -36,6 +36,17 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     // vai pegar um dto e instanciar um usuário
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
