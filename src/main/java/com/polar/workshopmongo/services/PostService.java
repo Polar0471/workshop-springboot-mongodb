c3549890cf3c1e66ea612a814c11768d3d716a49
Polar0471 <rodrigoarth512@gmail.com>
2023-11-14 15:28:36 -0300

    Obtendo um post por id

---
+++ src/main/java/com/polar/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.polar.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.polar.workshopmongo.domain.Post;
+
+import com.polar.workshopmongo.repository.PostRepository;
+import com.polar.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    // instanciando automaticamente um objeto UserRepository
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
